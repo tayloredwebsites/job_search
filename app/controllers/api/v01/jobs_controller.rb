7c5710ece8743a5a33b94143a52682e85bb2c86e
David Taylor <tayloredwebsites@me.com>
2017-11-09 17:55:35 -0500

    API model and controller tests

--- app/controllers/api/v01/jobs_controller.rb
+++ app/controllers/api/v01/jobs_controller.rb
@@ -9,24 +9,49 @@ class Api::V01::JobsController < ApplicationController
 
   # Job listing - app/javascript/components/job_listing.jsx
   def index
-    respond_with Job.all
+    # respond_with :api, :v01, Job.all
+    jobs = Job.all
+    render :json => jobs.to_json, status: 200
   end
 
   # Create Job - app/javascript/components/new_job.jsx
+  # todo - handle errors in front end
   def create
-    respond_with :api, :v01, Job.create(job_params)
+    # respond_with :api, :v01, Job.create(job_params)
+    job = Job.create(job_params)
+    # no error checks, so simply return
+    render :json => job.to_json
   end
 
   # Delete Job - app/javascript/components/body.jsx
   def destroy
-    respond_with Job.destroy(params[:id])
+    begin
+      job = Job.destroy(params[:id])
+    rescue StandardError => e
+      # capture exception on destroy
+      render :json => { :errors => e.message }, status: 422
+    else
+      # if not error not rescued (if job is returned as frozen/true), return 200 (OK with data)
+      # else error return 422 - Unprocessable Entity
+      # note HTTP destroy does not return data
+      render :json => {}, status: (job ? 204 : 422)
+    end
   end
 
   # Edit/Update Job - app/javascript/components/job.jsx
   def update
-    job = Job.find(params[:id])
-    job.update_attributes(job_params)
-    respond_with job, json: job
+    begin
+      job = Job.find(params[:id])
+      job.update_attributes(job_params)
+    rescue StandardError => e
+      # capture exception on update
+      render :json => { :errors => e.message }, status: 422
+    else
+      # if no exception on update, return status based upon error count
+      # note HTTP put does not return data
+      # to do - return errors to user interface
+      render :json => {}, status: (job.errors.count == 0 ? 204 : 422)
+    end
   end
 
   private
--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    # puts("+++ setup completed +++")
+  end
+
+  test "can create, find and delete job" do
+    job = Job.create( recruiter: 'Recruiter A', company: 'Company A')
+    assert_equal(0, job.errors.count)
+    begin
+      jobF = Job.find(job.id)
+    rescue
+      assert_equal('JobTest - Error finding job', 'Should not happen')
+    else
+      assert_equal(0, jobF.errors.count)
+    end
+    jobD = Job.delete( id: job.id)
+    assert(jobD)
+  end
+
 end
